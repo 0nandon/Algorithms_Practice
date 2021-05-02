e9fa9e39dcd3dae6e07a5e1d0f5e238c644f3018
onandon <71716834+0nandon@users.noreply.github.com>
2021-05-02 20:18:49 +0900

    Add files via upload

---
+++ algospot/Tree/TRAVERSAL.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int preorder[100];
+int inorder[100];
+int nodeNum;
+
+void traversal(int p1, int p2, int i1, int i2){
+    if(p1 <= p2 && i1 <= i2){
+        int cnt = 0;
+        while(inorder[i1+cnt] != preorder[p1]) cnt++;
+        
+        traversal(p1+1, p1+cnt, i1, i1+cnt-1);
+        traversal(p1+cnt+1, p2, i1+cnt+1, i2);
+        printf("%d ", preorder[p1]);
+    }
+}
+
+int main() {
+	int caseNum;
+    scanf("%d", &caseNum);
+    
+    for(int i=0; i<caseNum; i++){
+        scanf("%d", &nodeNum);
+        
+        for(int j=0; j<nodeNum; j++)
+            scanf("%d", &preorder[j]);
+        for(int j=0; j<nodeNum; j++)
+            scanf("%d", &inorder[j]);
+        
+        traversal(0, nodeNum-1, 0, nodeNum-1);
+        printf("\n");
+    }
+
+	return 0;
+}
+
