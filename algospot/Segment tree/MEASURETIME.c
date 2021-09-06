a0b6088004c73d9b12a14aab186296eee4ca70fe
onandon <71716834+0nandon@users.noreply.github.com>
2021-09-06 23:39:36 +0900

    Update MEASURETIME.c

--- algospot/Segment tree/MEASURETIME.c
+++ algospot/Segment tree/MEASURETIME.c
@@ -15,35 +15,35 @@ int sort[MAX];
 void merge(int arr[], int start, int half, int end){
 	int temp[end-start+1];
 	for(int i = 0; i<end-start+1; i++)
-		temp[i] = arr[start+i];
+	    temp[i] = arr[start+i];
 	
 	int cnt = 0, left = 0, right = 0;
 	while(left < half+1 && right < end-start-half){
-		if(temp[left] > temp[half+1+right]){
-			arr[start+cnt] = temp[half+1+right]; 
-            right++; cnt++;
-        }else{
-            arr[start+cnt] = temp[left]; 
-            left++; cnt++;
-        }
+	    if(temp[left] > temp[half+1+right]){
+	        arr[start+cnt] = temp[half+1+right]; 
+                right++; cnt++;
+            }else{
+                arr[start+cnt] = temp[left]; 
+                left++; cnt++;
+            }
 	}
 	
 	if(left == half+1)
-		for(int i = 0; i<end-start-half-right; i++)
-			arr[start+cnt+i] = temp[half+1+right+i];
+	    for(int i = 0; i<end-start-half-right; i++)
+		arr[start+cnt+i] = temp[half+1+right+i];
 	else if(right == end-start-half)
-		for(int i = 0; i<half+1-left; i++)
-			arr[start+cnt+i] = temp[left+i];
+	    for(int i = 0; i<half+1-left; i++)
+		arr[start+cnt+i] = temp[left+i];
 }
 
 // 병합정렬 구현 소스
 void mergeSort(int arr[], int start, int end){
     if(start != end){
-	    int half = (end-start)/2;
-	    mergeSort(arr, start, start+half);
+	int half = (end-start)/2;
+	mergeSort(arr, start, start+half);
         mergeSort(arr, start+half+1, end);
         merge(arr, start, half, end);
-	}
+    }
 }
 
 // tree를 모두 0으로 초기화한다.
